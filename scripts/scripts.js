d5101a52b25d72863efffff559da6b03e2617c46
Kim Cordova <cordova.k529@gmail.com>
2017-10-04 18:46:38 -0400

    save

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,9 @@ module.exports = function(droid06) {
 		return res.send("Hi there!");
 	});
 }
+
+module.exports = function(droid06) {
+	droid06.hear(/Hello!/, function(res) {
+		return res.send("Hi there!");
+	});
+}

09a5b9efc21a98467244917fdfad58c964c52a16
Kim Cordova <cordova.k529@gmail.com>
2017-10-09 22:11:53 -0400

    testing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,20 +4,15 @@ module.exports = function(droid06) {
 	});
 }
 
-module.exports = function(droid06) {
-	droid06.respond(/Tell me a joke/, function(res) {
-		return res.send("Want to hear a joke about the newspaper? Nevermind... it's tearable!");
-	});
-}
+// droid06.respond(/Tell me a joke/, function(res) {
+// 	return res.send("Want to hear a joke about the newspaper? Nevermind... it's tearable!");
+// });
 
-module.exports = function(droid06) {
-	droid06.respond(/Tell me another joke/, function(res) {
-		return res.send("How does a penguin build it’s house? Igloos it together.");
-	});
-}
 
-module.exports = function(droid06) {
-	droid06.hear(/guys/, function(res) {
-		return res.send("Why not try 'buds' instead?");
-	});
-}
+// droid06.respond(/Tell me another joke/, function(res) {
+// 	return res.send("How does a penguin build it’s house? Igloos it together.");
+// });
+
+// droid06.hear(/guys/, function(res) {
+// 	return res.send("Why not try 'buds' instead?");
+// });

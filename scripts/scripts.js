f2ae9b08b2b5a1d7412db0c43ef4cc2207a888e6
Kim Cordova <cordova.k529@gmail.com>
2017-10-09 22:16:51 -0400

    test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,13 @@ module.exports = function(droid06) {
 	droid06.hear(/Hello!/, function(res) {
 		return res.send("Hi there!");
 	});
-}
+
 
 droid06.respond(/Tell me a joke/, function(res) {
 	return res.send("Want to hear a joke about the newspaper? Nevermind... it's tearable!");
 });
 
-
+}
 // droid06.respond(/Tell me another joke/, function(res) {
 // 	return res.send("How does a penguin build it’s house? Igloos it together.");
 // });

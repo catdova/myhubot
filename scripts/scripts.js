ce639f5173ad6443a6ece5896cb94e7102d484c2
Kim Cordova <cordova.k529@gmail.com>
2017-10-09 22:02:01 -0400

    adding listen & respond scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,19 @@ module.exports = function(droid06) {
 }
 
 module.exports = function(droid06) {
-	droid06.hear(/Hello!/, function(res) {
-		return res.send("Hi there!");
+	droid06.respond(/Tell me a joke/, function(res) {
+		return res.send("Want to hear a joke about the newspaper? Nevermind... it's tearable!");
+	});
+}
+
+module.exports = function(droid06) {
+	droid06.respond(/Tell me another joke/, function(res) {
+		return res.send("How does a penguin build it’s house? Igloos it together.");
+	});
+}
+
+module.exports = function(droid06) {
+	droid06.hear(/guys/, function(res) {
+		return res.send("Why not try 'buds' instead?");
 	});
 }

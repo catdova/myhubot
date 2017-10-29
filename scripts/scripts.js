1abfe6e4e8f573f0953d8fdfc3cdd153a93a8d05
Kim Cordova <cordova.k529@gmail.com>
2017-10-29 14:37:08 -0400

    incremental commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,42 +15,41 @@ module.exports = function(droid06) {
 	droid06.hear(/Im sad/, function(res) {
 		return res.send("Maybe this will cheer you up! Why can't you hear a pterodactyl go to the bathroom? The p is silent.");
 	});
-/*
-	droid06.respond(/tell me a knock knock joke/), function(msg){
-		return msg.send("Knock knock!");
+
+	droid06.respond(/tell me a knock knock joke/), function(res){
+		return res.send("Knock knock!");
 	}
 
-	droid06.respond(/who's there/), function(msg){
+	droid06.respond(/who's there/), function(res){
 		var whosThere = ["Amos","Woo", "Hutch", "Radio","Ida"];
 		var rand = whosThere[Math.floor(Math.random() * whosThere.length)];
-		return msg.send(rand)
+		return res.send(rand)
 	}
 
-	droid06.respond(/(.*) who/, function(msg) {
-  	var punchline;
-  	punchline = msg.match[1];
- 	console.log(punchline);
-  	switch (punchline) {
-	  	case "Amos":
-	   		return msg.reply("A mosquito bit me!");
-	   		break;
-		case "Woo":
-			return msg.reply("Don't get too excited, it's only a joke.");
-  			break;
-		case "Hutch":
-			return msg.reply("Bless you!");
-		 	break;
-		case "Radio":
-			return msg.reply("Radio not, here I come!");
-			break;
-		case "Ida":
-			return msg.reply("Ida like to be your friend!");
-			break;
-		default:
-			return msg.reply("you ruined my joke");
-	  }
-});
+	droid06.respond(/(.*) who/, function(res) {
+	  	var punchline;
+	  	punchline = res.match[1];
+	 	console.log(punchline);
+	  	switch (punchline) {
+		  	case "Amos":
+		   		return res.reply("A mosquito bit me!");
+		   		break;
+			case "Woo":
+				return res.reply("Don't get too excited, it's only a joke.");
+	  			break;
+			case "Hutch":
+				return res.reply("Bless you!");
+			 	break;
+			case "Radio":
+				return res.reply("Radio not, here I come!");
+				break;
+			case "Ida":
+				return res.reply("Ida like to be your friend!");
+				break;
+			default:
+				return res.reply("you ruined my joke");
+		}
+	});
 
-*/
 };
 

1717d31fca681e83aa6482220d01603d502b1877
Kim Cordova <cordova.k529@gmail.com>
2017-10-29 15:08:37 -0400

    incremental commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,14 +20,14 @@ module.exports = function(droid06) {
 		return res.send("Knock knock!");
 	});
 
-	droid06.respond(/whos there/, function(res){
+	droid06.respond(/who's there/, function(res){
 		var whosThere = ["Amos","Woo", "Hutch", "Radio","Ida"];
 		console.log(whosThere);
 		var rand = whosThere[Math.floor(Math.random() * whosThere.length)];
 
 		return res.send(rand);
 	});
- /*
+ 
 	droid06.respond(/(.*)+ who/, function(res) {
 	  	var punchline;
 	  	punchline = res.match[1];
@@ -53,6 +53,6 @@ module.exports = function(droid06) {
 				break;
 		}
 	});
-*/
+
 };
 
